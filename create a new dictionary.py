1fb5e8b21f65926506531fea9edaa9ced2a04ea4
Rudraksh Rawal <144530387+Rudrakshrawal@users.noreply.github.com>
2023-12-17 00:06:12 +0530

    Create create a new dictionary.py

---
+++ create a new dictionary.py
@@ -0,0 +1,10 @@
+def print_cube_dictionary():
+  cube_dict = {num: num ** 3 for num in range(1, 6)}
+  print(cube_dict)
+
+# Calling the function to generate and print the dictionary
+print_cube_dictionary()
+
+
+#output
+#       {1: 1, 2: 8, 3: 27, 4: 64, 5: 125}
