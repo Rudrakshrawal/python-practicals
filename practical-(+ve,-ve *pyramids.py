9c9d73e1d61855e52c1c0cfcd2e0a32c766111a5
Rudraksh Rawal <144530387+Rudrakshrawal@users.noreply.github.com>
2023-12-16 19:04:18 +0530

    Create practical-(+ve,-ve *pyramids.py

---
+++ practical-(+ve,-ve *pyramids.py
@@ -0,0 +1,28 @@
+l = True
+while l ==True:
+  n=int(input("The rows for the pyramid is?: "))
+  i=int(1)
+  while n!=0:
+    print("*" * i, "\n")
+    n-=1
+    i+=2
+  l=input("wanna play again?(y/n)")
+  if l == "y":
+    l=bool(1)
+  else:
+    l=bool(0)
+
+
+#reverse pyramid
+l = True
+while l ==True:
+  n=int(input("The rows for the pyramid is?: "))
+  i=int(1)
+  while n!=0:
+    print("* "*(2*n - 1))
+    n-=1
+  l=input("wanna play again?(y/n)")
+  if l == "y":
+    l=bool(1)
+  else:
+    l=bool(0)
