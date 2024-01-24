f1d13ab3739c776a6d839510fb32233c612fd4a3
Rudraksh Rawal <144530387+Rudrakshrawal@users.noreply.github.com>
2024-01-25 01:33:09 +0530

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -7,4 +7,4 @@ password=""
 for i in range(length):
   password+=random.choice(chars)
 print(password)
-#Ransom is not actually random, rather its based on a "seed value" with the help of which the ranaom works.
+#Random is not actually random, rather its based on a "seed value" with the help of which the ranaom works.
